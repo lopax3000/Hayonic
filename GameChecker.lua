cd456ef11ea5c1985d6eb17d0d35c8bd50fd9e52
lopax3000 <108293666+lopax3000@users.noreply.github.com>
2022-07-04 17:32:38 +0100

    Update GameChecker.lua

--- GameChecker.lua
+++ GameChecker.lua
@@ -1,1 +1,8 @@
+local placeIDs = {
+        2629642516, -- Bitcoin Miner
+}
 
+
+if game.PlaceId == 2629642516 then
+           loadstring(game:HttpGet("https://raw.githubusercontent.com/lopax3000/Hayonic/main/BitcoinMiner.lua"))()
+  end
